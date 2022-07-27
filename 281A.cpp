fcb38e70f445fcc20a31ff59d9a7e56b8aae866f
MarilynBG <abravog1301@alumno.ipn.mx>
2022-07-26 23:56:33 -0500

    se agrega ejercicio

---
+++ 281A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define endl "\n"
+#define sp " "
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	string s;
+	cin>>s;
+	if(s[0]>96)
+		s[0]-=32;
+	cout<<s<<endl;
+
+	return 0;
+}
